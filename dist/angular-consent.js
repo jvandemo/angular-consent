496e8ee2374d180a8756b351f7b8c7a66a8bc52a
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-08-28 05:48:23 +0200

    feat(dist): update build

---
+++ dist/angular-consent.js
@@ -0,0 +1,84 @@
+(function (angular) {
+
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
+
+  // Config
+  angular.module('angularConsent.config', [])
+      .value('angularConsent.config', {
+          debug: true
+      });
+
+  // Modules
+  angular.module('angularConsent.directives', []);
+  angular.module('angularConsent.controllers', []);
+  angular.module('angularConsent',
+      [
+          'angularConsent.config',
+          'angularConsent.directives',
+          'angularConsent.controllers',
+          'ngCookies'
+      ]);
+
+})(angular);
+
+(function (angular) {
+
+  angular
+    .module('angularConsent.controllers')
+    .controller('angularConsent.ConsentController', ConsentController);
+
+  function ConsentController($cookies, $attrs){
+
+    this.getCookieKey = function(){
+      return 'angular-consent.' + ($attrs.consent || 'global');
+    };
+
+    this.getCookieValue = function(){
+      return $cookies.get(this.getCookieKey());
+    };
+
+    this.setCookieValue = function(value){
+      return $cookies.put(this.getCookieKey(), value);
+    };
+
+    this.hasAlreadyAgreed = function(){
+      if(this.getCookieValue()){
+        return true;
+      }
+      return false;
+    };
+
+    this.hasNotAgreedYet = function(){
+      return !this.hasAlreadyAgreed();
+    };
+
+    this.reset = function(){
+      $cookies.remove(this.getCookieKey());
+    };
+
+    this.agree = function(){
+      this.setCookieValue(Date.now());
+    };
+  }
+
+  ConsentController.$inject = ['$cookies', '$attrs'];
+
+})(angular);
+
+(function (angular) {
+
+  angular
+    .module('angularConsent.directives')
+    .directive('consent', createDirectiveDDO);
+
+  function createDirectiveDDO(){
+    return {
+      restrict: 'A',
+      controller: 'angularConsent.ConsentController',
+      controllerAs: '$consent'
+    };
+  }
+
+})(angular);
